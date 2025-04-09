1cac48f48db79e6c75bcd4a59b7013ec624e4bf2
vamsi <munagalavamsi37@gmail.com>
2025-04-09 09:44:47 +0530

    gnicorn app

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def predict():
     
     return jsonify(response)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000, debug=True)
